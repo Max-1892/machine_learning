835a90cb04a144e3fac36b62078775b2701abd85
Max Henry <somethingfunny14@gmail.com>
2016-08-31 22:53:17 -0400

    Created winnow_model class

--- module_1/main.py
+++ module_1/main.py
@@ -1,1 +1,7 @@
 from winnow_model import *
+model = WinnowModel(2, 0.5, 3, 1)
+f = open('test', 'r')
+for line in f:
+    line_split = line.split(',')
+    model.learn(map(int, line_split[:-1]), int(line_split[-1]))
+print model.output_model()
--- module_1/winnow_model.py
+++ module_1/winnow_model.py
@@ -1,11 +1,37 @@
 class WinnowModel:
+    """This class encapsulates the winnow-2 algorithm."""
     weights = []
     alpha = 2
     theta = len(weights) / 2
 
-    def __init__(self):
-        print "here"
+    def __init__(self, alpha, theta, num_of_weights, weight_initial_val):
+        self.alpha = alpha
+        self.theta = theta
+        for idx in range(num_of_weights):
+            self.weights.append(float(weight_initial_val))
 
-    def info(self):
-        print(weights, alpha, theta)
+    def learn(self, data_instance, label):
+        weight_instance_sum = 0
+        for idx, val in enumerate(self.weights):
+            weight_instance_sum += self.weights[idx] * data_instance[idx]
+        prediction = 1 if weight_instance_sum > self.theta else 0
+        if prediction == 1 and label == 0:
+            self.demote_weights(data_instance)
+        elif prediction == 0 and label == 1:
+            self.promote_weights(data_instance)
 
+    def promote_weights(self, data_instance):
+        for idx, val in enumerate(data_instance):
+            if val == 1:
+                self.weights[idx] = self.weights[idx] * self.alpha
+
+    def demote_weights(self, data_instance):
+        for idx, val in enumerate(data_instance):
+            if val == 1:
+                self.weights[idx] = self.weights[idx] / self.alpha
+
+    def output_model(self):
+        print self.weights
+        return "Weights: [%s], alpha: %f, theta: %f" % \
+            (",".join([str(weight) for weight in self.weights]), \
+            self.alpha , self.theta)
