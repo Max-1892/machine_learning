7a359011637280ae578711701620562e1f0310da
Max Henry <somethingfunny14@gmail.com>
2016-09-02 12:30:00 -0400

    now fill in incomplete instances based on conditional probability given class

--- module_1/preprocess_data.py
+++ module_1/preprocess_data.py
@@ -5,17 +5,34 @@ import os
 my_data = np.genfromtxt('cancer_data.txt', \
     delimiter=',', dtype='int32')
 
-# If an element can't be read in (i.e. is -1)
-# randomly choose a number from the rest of attribute
-# values for that column
-for ind, elem in np.ndenumerate(my_data):
+# Split instances into classes
+incomplete_instances = []
+negative_instances = []
+positive_instances = []
+for row in my_data:
+    if -1 in row:
+        incomplete_instances.append(row)
+    elif row[-1] == 0:
+        negative_instances.append(row)
+    elif row [-1] == 1:
+        positive_instances.append(row)
+incomplete_instances = np.array(incomplete_instances)
+negative_instances = np.array(negative_instances)
+positive_instances = np.array(positive_instances)
+
+# Fill in missing attribute data by randomly 
+# choosing an attribute value based on the
+# conditional probability given the class
+for idx, elem in np.ndenumerate(incomplete_instances):
     if elem == -1:
-        random_attr_val = -1
-        while (random_attr_val == -1):
-            random_attr_val = np.random.choice(my_data[:,ind[1]])
-        my_data[ind] = random_attr_val
+        if incomplete_instances[idx[0],-1] == 0:
+            random_attr_val = np.random.choice(negative_instances[:,idx[1]])
+        elif incomplete_instances[idx[0],-1] == 1:
+            random_attr_val = np.random.choice(positive_instances[:,idx[1]])
+        incomplete_instances[idx] = random_attr_val
 
 np.savetxt('test.out', my_data, delimiter=',', fmt='%d')
+
 # Convert discrete attributes into boolean ones
 # For the cancer data, each attribute can
 # range from 1 - 10 so we need 10 * 9 "new" boolean
@@ -23,7 +40,6 @@ np.savetxt('test.out', my_data, delimiter=',', fmt='%d')
 converted_result = ""
 file_in = open('test.out', 'r')
 file_out = open('cancer_data_encoded.txt', 'w')
-
 for line in file_in:
     split_line = line.split(',')
     for attr in split_line[:-1]:
